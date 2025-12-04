89b9e65b747d10290406da180acdb8b75b8dbd77
Laxmiikhanal <pujakhanal633@gmail.com>
2025-12-04 20:18:15 +0545

    sixth commit

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,8 @@
 // import 'package:flower_delivery_app/screens/loginpage_screen.dart';
 // import 'package:flower_delivery_app/screens/registerpage_screen.dart';
 // import 'package:flower_delivery_app/screens/Discoverflower_screen.dart';
-import 'package:flower_delivery_app/screens/fastflowerdelivery_screeen.dart';
+// import 'package:flower_delivery_app/screens/fastflowerdelivery_screeen.dart';
+import 'package:flower_delivery_app/screens/homepage_screen.dart';
 // import 'package:flower_delivery_app/screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -11,7 +12,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:FastDeliveryPage()
+      home:HomePage()
      );
   }
 }
