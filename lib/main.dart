da04500857415025f0a4bee950e3877457f74e7c
Laxmiikhanal <pujakhanal633@gmail.com>
2026-01-01 17:01:35 +0545

    COMMIT

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,23 @@
 import 'package:flower_delivery_app/app.dart';
-import'package:flutter/material.dart';
-void main(){
-  //material app ->parent container:1
-  //scaffold ->screen->jati screen hunxa
-  runApp(App());
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
+import 'features/auth/data/models/user_model.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Hive
+  await Hive.initFlutter();
+
+  // Register adapter (typeId = 1)
+  if (!Hive.isAdapterRegistered(1)) {
+    Hive.registerAdapter(UserModelAdapter());
+  }
+
+  // Open boxes BEFORE runApp
+  await Hive.openBox<UserModel>('usersBox');
+  await Hive.openBox<String>('sessionBox');
+
+  runApp(const App());
 }
